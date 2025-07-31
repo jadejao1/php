21c343e78dc7bf2a383d06d984be654c768b4343
jadejao1 <jadejaharsh14@gmail.com>
2025-07-31 08:49:43 +0530

    Delete get_data.php

--- get_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name =$_GET['name']  ;
-$age = $_GET['age']  ;
-$emil = $_GET['email'];
-echo "hello buddy : $name <br>";
-echo "Your age is : $age <br>";
-echo "your Email is : $emil <br>";
-?>
