e2e93179cd74a83e7a620f33f5053b49e014b073
jadejao1 <jadejaharsh14@gmail.com>
2025-07-31 08:48:49 +0530

    Add files via upload
    this is an example to how to get data in html and how to print using php in backend

---
+++ Form/get_data.php
@@ -0,0 +1,8 @@
+<?php
+$name =$_GET['name']  ;
+$age = $_GET['age']  ;
+$emil = $_GET['email'];
+echo "hello buddy : $name <br>";
+echo "Your age is : $age <br>";
+echo "your Email is : $emil <br>";
+?>
